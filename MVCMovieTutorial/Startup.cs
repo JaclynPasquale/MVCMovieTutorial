4a3b5e9a9b2c10055e16ddbd17a5fcb60e0494a1
JaclynPasquale <jaclynpasquale@gmail.com>
2015-03-04 16:26:29 -0600

    Working through MVC tutorial

---
+++ MVCMovieTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMovieTutorial.Startup))]
+namespace MVCMovieTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
